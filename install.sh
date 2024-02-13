c049a54b7ef185db21f629b4400735c1849856ba
Nuzair <nuzer501@gmail.com>
2024-02-13 15:28:59 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-curl -sSL https://raw.githubusercontent.com/Nuzair46/BlockTheSpot-Mac/main/install.sh
+bash <(curl -sSL https://raw.githubusercontent.com/Nuzair46/BlockTheSpot-Mac/main/install.sh) -hu
